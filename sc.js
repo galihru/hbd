1c93cc09bcee424976bd6017f4f1d415469a2d86
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-02-13 22:01:31 +0700

    Update sc.js

--- sc.js
+++ sc.js
@@ -206,8 +206,8 @@ async function generateHtml() {
       <link rel="canonical" href="https://4211421036.github.io/hbd/">
       <link rel="manifest" href="manifest.webmanifest" crossorigin="use-credentials">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="Selamat Ulang Tahun!">
       <meta name="application-name" content="HBD">
+      <meta name="description" content="Selamat Ulang Tahun!">
       <meta name="keywords" content="Selamat Ulang Tahun!">
       <meta name="author" content="GALIH RIDHO UTOMO">
       <meta name="robots" content="index, follow">
