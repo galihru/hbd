ca426387737caf4787a085c9f51b82062f9302e7
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-02-14 19:17:10 +0700

    Update sc.js

--- sc.js
+++ sc.js
@@ -300,8 +300,8 @@ async function generateHtml() {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="keywords" content="Selamat Ulang Tahun!">
       <meta name="author" content="GALIH RIDHO UTOMO">
-      <meta name="color-scheme" content="dark light">
       <meta name="robots" content="index, follow">
+      <meta name="color-scheme" content="dark light">
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#edf4f8">
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#1e1e1e">
       <meta prefix="og: http://ogp.me/ns#" property="og:title" content="Selamat Ulang Tahun!">
