6bbd885152866f74a44265efc3729bb72dbca724
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-02-14 17:59:11 +0700

    Update sc.js

--- sc.js
+++ sc.js
@@ -295,6 +295,7 @@ async function generateHtml() {
       <link rel="canonical" href="https://4211421036.github.io/hbd/">
       <meta name="description" content="Selamat Ulang Tahun!">
       <meta name="application-name" content="HBD">
+      <meta name="bingbot" content="noarchive">
       <meta name="generator" content="HBD">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta name="keywords" content="Selamat Ulang Tahun!">
